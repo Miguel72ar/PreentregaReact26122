1d39ee4584a7925770c03d9913a0b3cb201df3e0
Miguel72ar <rodriguezma72@gmail.com>
2026-07-18 19:33:43 -0300

    actualizacon entrega final de nuevo

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { ItemList } from "../ItemList/ItemList";
+import { getByCategory, getProducts } from "../../services/productsService";
+import { useParams } from "react-router-dom";
+
+export const ItemListContainer = () => {
+  //tomamos "category" del useParams para poder filtrar
+  const { category } = useParams();
+
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    //Usanmos la funcion con 2 uso: trae todo o filtra si category existe
+    getByCategory(category)
+      .then((data) => setProducts(data))
+      .catch((err) => console.log("Hubo un error:", err))
+      .finally(() => setLoading(false));
+  }, [category]);
+  //👉No olvidar agregar "category" como variable de dependencias para que renderice nuevamente el container si cambia category
+  //En caso de usar el filtro por categorias
+
+  if (loading) return <p>Cargando...</p>;
+
+  return (
+    <section>
+      <ItemList products={products} />
+    </section>
+  );
+};
